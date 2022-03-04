fcb3eaf88efd483ed34d51ecf92dd289f6cc9a39
Juan Francisco Builes Montoya <66885280+juanfranciscobumo@users.noreply.github.com>
2022-03-04 09:39:54 -0500

    Se corrige el pom

--- src/main/java/co/com/test/youtube/pages/LookAtTheMenuPage.java
+++ src/main/java/co/com/test/youtube/pages/LookAtTheMenuPage.java
@@ -5,7 +5,7 @@ import net.serenitybdd.screenplay.targets.Target;
 public class LookAtTheMenuPage {
 
     public static final Target BTN_GUIDE_ICON = Target.the("Button menu").locatedBy("//div[@id='start']/yt-icon-button[2]");
-    public static final Target LIVE = Target.the("Button menu").locatedBy("//div[@id='guide-inner-content']/ytd-guide-renderer/div[1]/ytd-guide-section-renderer[3]/div/ytd-guide-entry-renderer[5]");
+    public static final Target LIVE = Target.the("Button menu").locatedBy("//div[@id='guide-inner-content']/ytd-guide-renderer/div[1]/ytd-guide-section-renderer[3]/div/ytd-guide-entry-renderer//yt-formatted-string[.='Live' or .='En vivo']");
     public static final Target TEXT_LIVE = Target.the("Button menu").locatedBy("//div[@id='inner-header-container']/div[1]/ytd-channel-name/div");
 
 }
